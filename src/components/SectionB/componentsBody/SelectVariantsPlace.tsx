e34157b1b8bd8f9af79fdf95b2448efae7acd206
Leopoldo Caballero <git@pitakill.net>
2022-06-01 17:50:25 -0500

    Clean up warning in the console

--- src/components/SectionB/componentsBody/SelectVariantsPlace.tsx
+++ src/components/SectionB/componentsBody/SelectVariantsPlace.tsx
@@ -11,9 +11,7 @@ interface SelectVariantsPlaceProps {
 }
 
 const SelectVariantsPlace = ({ label }: SelectVariantsPlaceProps) => {
-  const [value, setValue] = React.useState<Date | null>(null);
-
-  const [option, setOption] = React.useState('');
+  const [option, setOption] = React.useState<string | undefined>();
 
   const handleChange = (event: SelectChangeEvent) => {
     setOption(event.target.value);
--- src/components/SectionB/componentsBody/SelectVariantsPregnantWoman.tsx
+++ src/components/SectionB/componentsBody/SelectVariantsPregnantWoman.tsx
@@ -11,9 +11,7 @@ interface SelectVariantsPregnantWomanProps {
 }
 
 const SelectVariantsPregnantWoman = ({ label }: SelectVariantsPregnantWomanProps) => {
-  const [value, setValue] = React.useState<Date | null>(null);
-
-  const [option, setOption] = React.useState('');
+  const [option, setOption] = React.useState<string | undefined>();
 
   const handleChange = (event: SelectChangeEvent) => {
     setOption(event.target.value);
